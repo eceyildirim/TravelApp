abba68f3de5fac4fc383954286cd42df2508187e
Ece Yıldırım <ece.y17@windowslive.com>
2018-07-23 11:28:32 +0300

    Add update and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-
-get '/tags' => 'tags#index'
-get '/tags/:id' => 'tag#show', as: :tag
+	get '/tags' => 'tags#index'
+	get '/tags/:id' => 'tags#show', as: :tag
+	get '/destinations/:id' => 'destinations#show', as: :destination
+	get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination 
+	patch '/destinations/:id' => 'destinations#update'
 end
+
